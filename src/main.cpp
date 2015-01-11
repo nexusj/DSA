1c9702bfee27cdeefbbb70479f84d5a6ff71116f
nexusj <ze_rat@hotmail.it>
2015-01-12 00:21:35 +0100

    Added Graph test :-1:

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,68 @@
 #include "PointerBinTree.h"
 #include "ListNTree.h"
 #include "ListPriorityQueue.h"
+#include "GrafoList.h"
 
 
 
 int main(int argc, char** argv)
 {
-	
+	using namespace std;
+	GrafoList<string, double> G(10);
 
+	NodoG n1, n2, n3, n4, n5, n6;
+
+	G.insNodo(n1);
+	G.scriviEtichetta(n1, "a");
+	G.insNodo(n2);
+	G.scriviEtichetta(n2, "b");
+	G.insNodo(n3);
+	G.scriviEtichetta(n3, "c");
+	G.insNodo(n4);
+	G.scriviEtichetta(n4, "d");
+	G.insNodo(n5);
+	G.scriviEtichetta(n5, "e");
+	G.insNodo(n6);
+	G.scriviEtichetta(n6, "f");
+
+
+
+	G.insArco(n1, n2, 1.0);
+	G.insArco(n1, n3, 0.9);
+	G.insArco(n1, n5, 0.3);
+	G.insArco(n2, n4, 0.1);
+	G.insArco(n2, n6, 0.2);
+	G.insArco(n3, n4, 1.0);
+	G.insArco(n3, n6, 0.7);
+	G.insArco(n4, n1, 0.5);
+	G.insArco(n5, n2, 0.4);
+	G.insArco(n5, n3, 0.2);
+	G.insArco(n6, n4, 0.1);
+
+	cout << "\nNumNodi " << G.numNodi();
+	cout << "\nNumArchi " << G.numArchi();
+
+	G.cancNodo(n6);
+
+	cout << "\nNumNodi " << G.numNodi() << endl;
+
+	GrafoList<string, double>::ListaNodi L = G.Adiacenti(n1);
+
+	GrafoList<string, double>::ListaNodiPos p;
+	GrafoList<string, double>::Arco a;
+
+	a.nodo1 = n1;
+	a.nodo2 = n4;
+
+	cout << "Adiacenti di " << G.leggiEtichetta(n1).c_str() << "--> ";
+	p = L.begin();
+	while (!L.end(p)){
+		cout << G.leggiEtichetta(*(L.read(p))).c_str() << " ";
+		p = L.next(p);
+	}
+
+	auto arc = G.esisteArco(a);
+	auto existnode = G.esisteNodo(*(new NodoG(5)));
 	ListS<int> t;
 	ListP<int> l;
 	StackP<int> s;
