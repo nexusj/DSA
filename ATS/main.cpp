834dd1c2a9267da471950d2fd5656a1d43e95b5b
nexusj <ze_rat@hotmail.it>
2015-02-13 20:38:54 +0100

    Testing new stuff.

--- ATS/main.cpp
+++ ATS/main.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
 #include "N-Queens.h"
 #include "StringMatching.h"
+#include "Knapsack.h"
 
 int main(int _Argc, char ** _Argv)
 {
-	NQueens queen(10);
+	
 
+	Knapsack knsack(3);
+	NQueens queen(10);
 	std::string pattern, text;
+
+
+	knsack.AddItem(*(new IData(6, 2)));
+	knsack.AddItem(*(new IData(4, 1)));
+	knsack.AddItem(*(new IData(7, 3)));
+
+	knsack.KnapsackGreedy(5);
+
+	
+	std::cout << "//STRING MATCHING//" << std::endl << std::endl;
+	
 
 	std::cout << "Inserire stringa testo: ";
 	std::cin >> text;
@@ -16,11 +30,15 @@ int main(int _Argc, char ** _Argv)
 
 	
 
-	std::cout << "Occorrenza trovata in posizione : " << CompareStrings(pattern, text) << std::endl;
+	std::cout << "Occorrenza trovata in posizione : " << CompareStrings(pattern, text) << std::endl << std::endl;
+
+	std::cout << "//N-QUEEN RESOLVER//" << std::endl << std::endl;
+
 	if (queen.Start())
 		queen.Print();
 	else
 		std::cout << "Configurazione non valida ";
+
 	std::cout << std::endl;
 	std::cout << "Premere un tasto per continuare ";
 
